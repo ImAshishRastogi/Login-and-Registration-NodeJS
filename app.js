098a7d72d63eb5f383a920250f04ac599a8d3f0d
Ashish Rastogi <rastogiashish52@gmail.com>
2019-03-13 11:29:07 +0530

    JWT update

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ var app = express();
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
+var URL = require('url').URL;
+
+
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
+var config = require('./config.js');
+var middleware = require('./middleware');
+
+
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/userdb',{ useNewUrlParser: true });
 
@@ -32,36 +41,68 @@ app.get("/",function(request,response) {
 
 app.get("/register",function(req,res) {
     console.log("enter in REGISTER PAGE")
+    
     if(req.session.user){
         console.log("SESSION exist for user : "+ req.session.user.user_name);
         console.log("Redirecting to LOGGEDIN PAGE ");
         res.redirect("/loggedin");
      }
+     
     res.sendFile(__dirname+"/"+"register_page.html");
 });
 
 
 app.get("/login",function(req,res) {
     console.log("enter in LOGIN PAGE")
+   
     if(req.session.user){
         console.log("SESSION exist for user : "+ req.session.user.user_name);
         console.log("Redirecting to LOGGEDIN PAGE ");
         res.redirect("/loggedin");
      }
+     
     res.sendFile(__dirname+"/"+"login_page.html");
 });
 
+function checkToken(req) {
+    console.log(req)
+    var token =req.get('authorization');
+    console.log(token);
+    if(token) {
+        jwt.verify(token, config.secret, function(err, decoded) {
+            if(err) {
+                return res.json({
+                    success : false,
+                    message : 'Token is not valid'
+                });
+            } else {
+                return token
+            }
+        })
+    } else {
+        console.log("TOKEN doesn't exist for any user. \n Redirecting to LOGIN PAGE ");
+        res.redirect("/login");
+    }
+}
 app.get("/loggedin",function(req,res) {
+    
     console.log("enter  in LOGGEDIN PAGE")
     if(req.session.user){
         console.log("logged in user : "+ req.session.user.user_name);
-        res.send("<head><title>USER</title></head><body><font color=green>"+req.session.user.user_name+
-        " loggedn in<font><br /><a href=/logout> LOGOUT<a></body>");
+        res.send("<head><title>HOME</title></head><body><font color=green>"+req.session.user.user_name+
+        " loggedn in<font><br /><a href=/profile> PROFILE<a></body><br /><a href=/logout> LOGOUT<a></body>");
      } else { 
         console.log("SESSION doesn't exist for any user. \n Redirecting to LOGIN PAGE ");
         res.redirect("/login");
      }
+     
 });
+
+app.get("/profile",function(req,res) {
+    var token = jwt.sign({id: req.session.user.user_name},config.secret,{expiresIn : 60});
+    console.log(token)
+    res.send("<head><title>PROFILE</title></head><body><font color=green> "+req.session.user.user_name+"<font><br /><a href=/logout> LOGOUT<a></body>");
+})
 
 app.post("/register-submit", function(req ,res) {
     console.log("enter in REGISTER POST REQUEST(register-submit)");
@@ -72,19 +113,20 @@ app.post("/register-submit", function(req ,res) {
      } else {
          userDB.findOne({user_name : req.body.user_name},function(err,result) {
              if(result==null) {
+                //var hashedPassword = bcrypt.hashSync(req.body.password, 8);
                 var newUser=new userDB({user_name: req.body.user_name ,password : req.body.password, email : req.body.email});
                 newUser.save(function(err,userDB) {
                     if(err) console.error("ERROR in save() function : "+err.message);
                     else{
                         console.log("SUCCESSFUL REGISTER USER ");
                         console.log(result)
-                    res.send("<font color=green>"+req.body.user_name+" Successful Registered </font><br /><a href='/login'>Go to Login page</a>");
+                        res.status(200);
+                        res.send("<font color=green>"+req.body.user_name+" Successful Registered </font><br /><a href='/login'>Go to Login page</a>");
                     }
                 })
              } else{
-                 console.log("User Already Exist !!!");
-                res.send("<font color=red> User Already Exists! Login or choose another user name</font><br /><a href='/register'>Register another user</a>");
-
+                    console.log("User Already Exist !!!");
+                    res.send("<font color=red> User Already Exists! Login or choose another user name</font><br /><a href='/register'>Register another user</a>");
              }
          })
      }
@@ -96,19 +138,32 @@ app.post("/check-login",function(req,res) {
     console.log("enter in LOGIN POST REQUEST(check-login)");
     if(!req.body.user_name || !req.body.password){
         console.log("INVALID DETAILS");
-        res.status("400");
         res.send("<font color=red>Invalid details!</font><br /><a href='/login'>BACK</a>");
      } else {
          userDB.findOne({user_name : req.body.user_name, password : req.body.password},{_id:0,user_name:1,password:1},function(err, response) {
              if(response==null ) {
-                 console.error("User : "+req.body.user_name+"has not found in DB !!!");
+                 console.error("Incorrect username or password!!!");
                  console.log("Redirecting to LOGIN PAGE");
                 res.redirect('/login');
             } else {
-                console.log("User : "+req.body.user_name+"has found in DB.");
+                console.log("User : "+req.body.user_name+" has found in DB.");
                 req.session.user=response;
                 console.log("Assigning SESSION to user : "+response.user_name);
-                console.log("Redirecting to LOGIN PAGE");
+                //var token = jwt.sign({id: response.user_name},config.secret,{expiresIn : 60});
+                //console.log(token)
+                /*
+                res.json({
+                    success : true,
+                    token : token
+                });
+                */
+                //res.setHeader('authorization',token); 
+                //res.set('x-access-token' , token)
+                //res.cookie('token',token);
+                //res.header('authorization', token).send();
+                //console.log(res)
+                //checkToken(res)
+                console.log("Redirecting to LOGGEDIN PAGE");
                 res.redirect("/loggedin");
              }
          })
@@ -117,12 +172,16 @@ app.post("/check-login",function(req,res) {
 
 app.get('/logout', function(req, res){
     console.log("enter in LOGOUT PAGE");
+    //console.log(res)
+    
     if(req.session.user) {
         var name=req.session.user.user_name;
     req.session.destroy();
     console.log("SESSION has been destroyed for user : "+name)
     console.log("User : "+name+" logged out.")
     }
+    
+
     console.log("Redirecting to LOGIN PAGE");
     res.redirect('/login');
  });
